d1d26294b7b8117572dba63f3f42bcc6d2dbc384
JC <dev@jacobcasper.com>
2025-05-15 09:42:16 -0500

    Add rudimentary level 90 monk support by updating potencies and openers

--- packages/core/src/sims/melee/mnk/mnk_actions.ts
+++ packages/core/src/sims/melee/mnk/mnk_actions.ts
@@ -251,6 +251,12 @@ export const Bootshine: FuryAbility = {
             gauge.beastChakra.push('opo');
         }
     },
+    levelModifiers: [{
+        minLevel: 92,
+        name: "Leaping Opo",
+        id: 36945,
+        potency: 260,
+    }],
 };
 
 export const TrueStrike: FuryAbility = {
@@ -269,6 +275,12 @@ export const TrueStrike: FuryAbility = {
             gauge.beastChakra.push('raptor');
         }
     },
+    levelModifiers: [{
+        minLevel: 92,
+        name: "Rising Raptor",
+        id: 36946,
+        potency: 340,
+    }],
 };
 
 export const SnapPunch: FuryAbility = {
@@ -287,6 +299,12 @@ export const SnapPunch: FuryAbility = {
             gauge.beastChakra.push('coeurl');
         }
     },
+    levelModifiers: [{
+        minLevel: 92,
+        name: "Pouncing Couerl",
+        id: 36947,
+        potency: 370, // assumed positional hit
+    }],
 };
 
 // I'm not implementing all 4 meditations lol
@@ -415,6 +433,12 @@ export const ElixirField: MnkGcdAbility = {
         gauge.beastChakra = [];
     },
     activatesBuffs: [FormlessFist],
+    levelModifiers: [{
+        minLevel: 92,
+        name: "Elixir Burst",
+        id: 36948,
+        potency: 900,
+    }],
 };
 
 export const FlintStrike: MnkGcdAbility = {
@@ -474,7 +498,11 @@ export const RiddleOfFire: MnkOgcdAbility = {
     cooldown: {
         time: 60,
     },
-    activatesBuffs: [RiddleOfFireBuff, FiresRumination],
+    activatesBuffs: [RiddleOfFireBuff],
+    levelModifiers: [{
+        minLevel: 100,
+        activatesBuffs: [RiddleOfFireBuff, FiresRumination],
+    }],
 };
 export const Brotherhood: MnkOgcdAbility = {
     name: "Brotherhood",
@@ -497,7 +525,11 @@ export const RiddleOfWind: MnkOgcdAbility = {
     cooldown: {
         time: 90,
     },
-    activatesBuffs: [RiddleOfWindBuff, WindsRumination],
+    activatesBuffs: [RiddleOfWindBuff],
+    levelModifiers: [{
+        minLevel: 96,
+        activatesBuffs: [RiddleOfWindBuff, WindsRumination],
+    }],
 };
 
 export const SixSidedStar: MnkGcdAbility = {
@@ -573,6 +605,7 @@ export const FiresReply: MnkGcdAbility = {
 };
 
 export const OPO_ABILITIES: number[] = [Bootshine.id, DragonKick.id, LeapingOpo.id];
+export const BOOTSHINE_ABILITIES: number[] = [Bootshine.id, LeapingOpo.id];
 const RAPTOR_ABILITIES: number[] = [TrueStrike.id, TwinSnakes.id, RisingRaptor.id];
 const COUERL_ABILITIES: number[] = [SnapPunch.id, Demolish.id, PouncingCoeurl.id];
 const FORM_ABILITIES: number[] = [Bootshine.id, DragonKick.id, LeapingOpo.id, TrueStrike.id, TwinSnakes.id, RisingRaptor.id, SnapPunch.id, Demolish.id, PouncingCoeurl.id];
